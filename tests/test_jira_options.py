fa7ccfa1784c6648d3dcb2ee877d18f41d0eeb49
casperlehmann <6682833+casperlehmann@users.noreply.github.com>
2025-04-12 19:39:38 +0200

    Fixtures

--- tests/test_jira_options.py
+++ tests/test_jira_options.py
@@ -4,33 +4,37 @@ from dataclasses import dataclass
 from jira import JiraOptions
 
 @pytest.fixture
-def fake_toml():
-    return '\n'.join((
+def fake_toml(tmpdir):
+    toml_contents = '\n'.join((
         '[jira]',
         'user = "user@domain.com"',
         'url = "https://account.atlassian.net"',
         'personal-access-token = "zxcv_JIRA_TOKEN"',
         ''
     ))
-
-def test_JiraOptions(tmpdir, fake_toml):
     toml = tmpdir / "options.toml"
-    toml.write(fake_toml)
-    opts = JiraOptions(toml_source = toml)
+    toml.write(toml_contents)
+    return toml
+
 @dataclass
-class cli:
+class Cli:
     user = 'admin@domain.com'
     jira_url = 'https://admin.atlassian.net'
     personal_access_token = 'SECRET'
     no_verify_ssl = False
 
+@pytest.fixture
+def fake_cli():
+    return Cli()
+
+def test_JiraOptions(fake_toml):
+    opts = JiraOptions(toml_source = fake_toml)
     assert opts.user == 'user@domain.com'
     assert opts.url == 'https://account.atlassian.net'
     assert opts.personal_access_token == 'zxcv_JIRA_TOKEN'
 
-def test_JiraOptionsOverride(tmpdir, fake_toml):
-    toml = tmpdir / "options.toml"
-    toml.write(fake_toml)
+def test_JiraOptionsOverride(fake_toml, fake_cli):
+    opts = JiraOptions(toml_source = fake_toml, parser = fake_cli)
     assert opts.user == 'admin@domain.com'
     assert opts.url == 'https://admin.atlassian.net'
     assert opts.personal_access_token == 'SECRET'
