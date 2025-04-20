54c055dc572513d4d9670969ac387fdac94f2cab
casperlehmann <6682833+casperlehmann@users.noreply.github.com>
2025-04-20 22:56:59 +0200

    Remove comments

--- mantis/jira/utils/jira_system_config_loader.py
+++ mantis/jira/utils/jira_system_config_loader.py
@@ -56,8 +56,6 @@ def fetch_enums(jira: 'JiraClient',
 class ProjectFieldKeys:
     def __init__(self, name: str, data: Mapping[str, list[Project]]) -> None:
         assert data
-        # print (data['projects'][0].keys())
-        # dict_keys(['expand', 'self', 'id', 'key', 'name', 'avatarUrls', 'issuetypes'])
         self.name = name
         self.data = data
 
@@ -79,8 +77,6 @@ class ProjectFieldKeys:
     def show(self) -> None:
         pprint(', '.join(self.fields))
         return self.fields
-        # for field in self.fields:
-        #     print (field)
 
 class JiraSystemConfigLoader:
     def __init__(self, client: 'JiraClient') -> None:
