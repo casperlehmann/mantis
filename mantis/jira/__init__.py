5bcb00f1acc7aac4e66dd557d2f2c9f591ca077e
Casper Lehmann <6682833+casperlehmann@users.noreply.github.com>
2025-04-20 23:49:04 +0200

    Introduce dedicated class for Jira Issue (#9)
    Start introducing better type coverage, starting with a new `JiraIssue`
    class.

--- mantis/jira/__init__.py
+++ mantis/jira/__init__.py
@@ -1,6 +1,6 @@
 from .jira_options import JiraOptions, parse_args
 from .jira_auth import JiraAuth
 from .jira_client import JiraClient
-from .jira_issues import JiraIssues
+from .jira_issues import JiraIssues, JiraIssue
 from .utils import JiraSystemConfigLoader
 
