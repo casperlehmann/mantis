0126d1e92e9e96d9473132917311c7313c8afcb5
casperlehmann <6682833+casperlehmann@users.noreply.github.com>
2025-04-23 11:07:55 +0200

    Apply auto-formatting to mantis

--- mantis/jira/__init__.py
+++ mantis/jira/__init__.py
@@ -1,6 +1,5 @@
-from .jira_options import JiraOptions, parse_args
 from .jira_auth import JiraAuth
 from .jira_client import JiraClient
-from .jira_issues import JiraIssues, JiraIssue
+from .jira_issues import JiraIssue, JiraIssues
+from .jira_options import JiraOptions, parse_args
 from .utils import JiraSystemConfigLoader
-
