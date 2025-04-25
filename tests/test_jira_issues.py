5ff7f6b7e7c9f04064befc7f19ac65a1e0222d58
casperlehmann <6682833+casperlehmann@users.noreply.github.com>
2025-04-25 15:58:21 +0200

    Test JiraIssues use of cache

--- tests/test_jira_issues.py
+++ tests/test_jira_issues.py
@@ -1,3 +1,4 @@
+import json
 import os
 import re
 from unittest.mock import Mock, patch
@@ -150,9 +151,24 @@ def test_jira_issues_cached_issuetypes_parses_allowed_types(fake_jira):
     assert fake_jira.issues.allowed_types == ["Bug", "Task"]
 
 
-def test_jira_issues_get_caches_jira_issue(with_fake_cache, fake_jira: JiraClient):
+def test_jira_issues_get_does_write_to_cache(with_fake_cache, fake_jira: JiraClient):
     fake_jira._no_cache = False
     assert fake_jira.cache.get_issue("TASK-1") is None
+    assert len([file for file in fake_jira.cache.issues.iterdir()]) == 0
     issue = fake_jira.issues.get("TASK-1")
     assert fake_jira.cache.get_issue("TASK-1")
-    assert issue.get("key") == "TASK-1"
+    assert len([file for file in fake_jira.cache.issues.iterdir()]) == 1
+    with open(fake_jira.cache.issues / "TASK-1.json", "r") as f:
+        data = json.load(f)
+    assert data == {"fields": {"status": {"name": "resolved"}}, "key": "TASK-1"}
+
+
+def test_jira_issues_get_does_retrieve_from_cache(
+    with_fake_cache, fake_jira: JiraClient
+):
+    fake_jira._no_cache = False
+    data = {"redacted": "True"}
+    with open(fake_jira.cache.issues / "TASK-1.json", "w") as f:
+        json.dump(data, f)
+    issue = fake_jira.issues.get("TASK-1")
+    assert issue.get("redacted") == "True"
