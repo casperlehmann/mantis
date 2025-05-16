449d06adc377310996d6ec6b9b89740bb72ba431
casperlehmann <6682833+casperlehmann@users.noreply.github.com>
2025-05-16 22:14:16 +0200

    Add repo_health functions

--- scripts/development-functions.sh
+++ scripts/development-functions.sh
@@ -15,6 +15,27 @@ run_coverage() {
   pytest --cov
 }
 
+run_mypy() {
+  mypy --disallow-untyped-calls --disallow-untyped-defs --disallow-incomplete-defs mantis
+}
+
+run_flake8_sparse() {
+  flake8 mantis --count --select=E9,F63,F7,F82 --show-source --statistics                 
+}
+
+run_flake8() {
+  flake8 mantis --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
+}
+
+repo_health() {
+    echo "\n# Running run_coverage"
+    run_coverage
+    echo "\n# Running run_mypy"
+    run_mypy
+    echo "\n# Running run_flake8_sparse"
+    run_flake8_sparse
+}
+
 show_coverage() {
   pytest --cov-report html --cov
   open htmlcov/index.html
