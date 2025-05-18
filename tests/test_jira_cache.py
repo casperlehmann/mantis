5351de6cf1ab6218fc43530826a3aef988ced233
casperlehmann <6682833+casperlehmann@users.noreply.github.com>
2025-05-18 22:44:03 +0200

    Everywhere uses fixture minimal_issue_payload

--- tests/test_jira_cache.py
+++ tests/test_jira_cache.py
@@ -1,28 +1,30 @@
+import json
 import pytest
 
 from mantis.jira import JiraClient
 
 
-def test_cache_get_caches_jira_issue(fake_jira: JiraClient):
+def test_cache_get_caches_jira_issue(fake_jira: JiraClient, minimal_issue_payload):
     assert not fake_jira._no_read_cache
     assert fake_jira.cache._get(fake_jira.cache.issues, "TASK-1.json") is None
 
     with open(fake_jira.cache.root / "issues/TASK-1.json", "w") as f:
-        f.write('{"fields": {"status": {"name": "resolved"}}, "key": "TASK-1"}')
+        json.dump(minimal_issue_payload, f)
 
     decoded = fake_jira.cache._get(fake_jira.cache.issues, "TASK-1.json")
-    assert decoded == {"fields": {"status": {"name": "resolved"}}, "key": "TASK-1"}
+    assert decoded
+    assert decoded.get("key", '') == "TASK-1"
 
 
-def test_cache_get_issue_returns_none_when_no_read_cache_is_set(fake_jira: JiraClient):
+def test_cache_get_issue_returns_none_when_no_read_cache_is_set(fake_jira: JiraClient, minimal_issue_payload):
     # Make sure nothing is cached
     assert not fake_jira._no_read_cache
     assert fake_jira.cache.get_issue("TASK-1") is None
 
     # cache something
-    with open(fake_jira.cache.root / "issues/task-1.json", "w") as f:
-        f.write('{"fields": {"status": {"name": "resolved"}}, "key": "task-1"}')
-    something = fake_jira.cache.get_issue("task-1")
+    with open(fake_jira.cache.root / "issues/TASK-1.json", "w") as f:
+        json.dump(minimal_issue_payload, f)
+    something = fake_jira.cache.get_issue("TASK-1")
     assert something is not None
 
     # Deactivate the cache and make sure nothing is retrieved
@@ -32,10 +34,10 @@ def test_cache_get_issue_returns_none_when_no_read_cache_is_set(fake_jira: JiraC
         assert nothing_2 is None
 
 
-def test_cache_remove_does_removals(fake_jira: JiraClient):
+def test_cache_remove_does_removals(fake_jira: JiraClient, minimal_issue_payload):
     # cache something
-    with open(fake_jira.cache.root / "issues/task-1.json", "w") as f:
-        f.write('{"fields": {"status": {"name": "resolved"}}, "key": "task-1"}')
+    with open(fake_jira.cache.root / "issues/TASK-1.json", "w") as f:
+        json.dump(minimal_issue_payload, f)
     something_1 = fake_jira.cache.get_issue("task-1")
     assert something_1 is not None
 
@@ -46,10 +48,10 @@ def test_cache_remove_does_removals(fake_jira: JiraClient):
     assert nothing_1 is None
 
 
-def test_cache_remove_issue_does_removals(fake_jira: JiraClient):
+def test_cache_remove_issue_does_removals(fake_jira: JiraClient, minimal_issue_payload):
     # cache something
-    with open(fake_jira.cache.root / "issues/task-1.json", "w") as f:
-        f.write('{"fields": {"status": {"name": "resolved"}}, "key": "task-1"}')
+    with open(fake_jira.cache.root / "issues/TASK-1.json", "w") as f:
+        json.dump(minimal_issue_payload, f)
     something_2 = fake_jira.cache.get_issue("task-1")
     assert something_2 is not None
 
