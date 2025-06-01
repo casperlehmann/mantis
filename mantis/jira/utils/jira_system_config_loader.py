52128601d0a89e4a28b5f2431986303481b5550d
casperlehmann <6682833+casperlehmann@users.noreply.github.com>
2025-06-01 10:15:05 +0200

    assert -> manual raises

--- mantis/jira/utils/jira_system_config_loader.py
+++ mantis/jira/utils/jira_system_config_loader.py
@@ -137,7 +137,12 @@ class CreatemetaModelFactory(MetaModelFactory):
 
     def __init__(self, metadata: Dict[str, Any]):
         super().__init__(metadata)
-        assert isinstance(self.meta_fields, list), f'CreatemetaModelFactory.meta_fields should be of type list. Got: {type(self.meta_fields)}'
+        if isinstance(self.meta_fields, dict):
+            raise ValueError('CreatemetaModelFactory.meta_fields should be of type list. '
+                             'Got dict. Did you accidentally pass be an "editmeta"?')
+        if not isinstance(self.meta_fields, list):
+            raise TypeError(
+                f'CreatemetaModelFactory.meta_fields should be of type list. Got: {type(self.meta_fields)}')
         self.create_model()
 
 
@@ -153,7 +158,12 @@ class EditmetaModelFactory(MetaModelFactory):
 
     def __init__(self, metadata: Dict[str, Any]):
         super().__init__(metadata)
-        assert isinstance(self.meta_fields, dict), f'EditmetaModelFactory.meta_fields should be of type dict. Got: {type(self.meta_fields)}'
+        if isinstance(self.meta_fields, list):
+            raise ValueError('EditmetaModelFactory.meta_fields should be of type dict. '
+                             'Got list. Did you accidentally pass be a "createmeta"?')
+        if not isinstance(self.meta_fields, dict):
+            raise TypeError(
+                f'EditmetaModelFactory.meta_fields should be of type dict. Got: {type(self.meta_fields)}')
         self.create_model()
 
 
