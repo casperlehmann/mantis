d9800c7cb13bc28336af9dd713872ea72043d314
casperlehmann <6682833+casperlehmann@users.noreply.github.com>
2025-06-02 08:34:56 +0200

    Test dir exists

--- tests/test_jira_client.py
+++ tests/test_jira_client.py
@@ -28,9 +28,9 @@ class TestJiraClient:
         assert len(list(fake_jira.cache.root.iterdir())) == 2, f'Iter root expected two values, got: {list_of}'
         for item in fake_jira.cache.root.iterdir():
             assert item.name in ("system", "issues")
-        assert len(list(fake_jira.cache.system.iterdir())) == 2
+        assert len(list(fake_jira.cache.system.iterdir())) == 3
         for item in fake_jira.cache.system.iterdir():
-            assert item.name in ("createmeta", "editmeta")
+            assert item.name in ("createmeta", "editmeta", 'createmeta_schemas')
 
     def test_get_current_user(self, fake_jira: JiraClient, requests_mock):
         requests_mock.get(f'{fake_jira.api_url}/myself', json=CacheData().placeholder_account)
