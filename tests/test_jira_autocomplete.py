b86989cf068bab239fae188e3d49db8fd25541a9
casperlehmann <6682833+casperlehmann@users.noreply.github.com>
2025-06-06 18:41:02 +0200

    Use CacheData for TestJiraAutocomplete

--- tests/test_jira_autocomplete.py
+++ tests/test_jira_autocomplete.py
@@ -1,9 +1,11 @@
 from mantis.jira.jira_client import JiraClient
+from tests.data import CacheData
 
 
 class TestJiraAutocomplete:
     def test_get_suggestions(self, fake_jira: JiraClient, requests_mock):
-        return_value = {'results': [{'value': 'abc123', 'displayName': '<b>Marcus</b> Aurelius - <b>marcus</b>@rome.gov'}]}
+        accountId = CacheData().placeholder_account['accountId']
+        return_value = {'results': [{'value': accountId, 'displayName': '<b>Marcus</b> Aurelius - <b>marcus</b>@rome.gov'}]}
         requests_mock.get(fake_jira.api_url + '/jql/autocompletedata/suggestions?fieldName=reporter&fieldValue=Marcus', json=return_value)
         suggestions = fake_jira.auto_complete.get_suggestions('reporter', 'Marcus')
         assert len(suggestions) == 1
