b3560f135143c6d9341b7949c19262f5e2e97bb0
casperlehmann <6682833+casperlehmann@users.noreply.github.com>
2025-06-27 22:16:23 +0200

    rename client to open_ai

--- src/mantis/openai_client.py
+++ src/mantis/openai_client.py
@@ -11,12 +11,12 @@ class OpenAIClient:
     def __init__(self, mantis: 'MantisClient') -> None:
         self.mantis = mantis
         self.disabled = not self.mantis.options.chat_gpt_activated
-        self.client = OpenAI(base_url=self.mantis.options.chat_gpt_base_url, api_key=self.mantis.options.chat_gpt_api_key)
+        self.open_ai = OpenAI(base_url=self.mantis.options.chat_gpt_base_url, api_key=self.mantis.options.chat_gpt_api_key)
         
     def get_completion(self, input_text: str, prompt: str, model: str = 'gpt-4.1') -> str:
         if self.disabled:
             raise ConnectionError('OpenAI connectivity has not been configured')
-        completion = self.client.chat.completions.create(
+        completion = self.open_ai.chat.completions.create(
                 model="gpt-4.1",
                 messages=[
                     {"role": "developer", "content": prompt},
