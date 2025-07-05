8b075748e3f732816f292b4342df064145cd2196
casperlehmann <6682833+casperlehmann@users.noreply.github.com>
2025-07-05 18:31:44 +0200

    Update code

--- src/jira/jira_client.py
+++ src/jira/jira_client.py
@@ -238,7 +238,7 @@ class JiraClient:
         except requests.exceptions.ConnectionError:
             print("Connection error. Run it like this:")
             print("export JIRA_TOKEN=$(cat secret.txt)")
-            print("poetry run python main.py")
+            print("uv run python main.py")
             exit(1)
         except Exception as e:
             print("test_auth failed for unknown reasons.")
